dac9981cb9afb4bcc99e8a43d3c2307c380ad4f8
Jianpan <497231226@qq.com>
2018-07-24 20:57:26 +0800

    分类管理模块

--- src/main/java/com/mmall/dao/CategoryMapper.java
+++ src/main/java/com/mmall/dao/CategoryMapper.java
@@ -1,6 +1,9 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Category;
+import sun.jvm.hotspot.jdi.IntegerTypeImpl;
+
+import java.util.List;
 
 public interface CategoryMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +17,6 @@ public interface CategoryMapper {
     int updateByPrimaryKeySelective(Category record);
 
     int updateByPrimaryKey(Category record);
+
+    List<Category> selectCategoryChildrenByParentId(Integer parentId);
 }
